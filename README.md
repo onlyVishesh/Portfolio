4230cc7adb9ad60a1e9dc3a874ff248d3c988483
onlyVishesh <Visheshmaurya14@gmail.com>
2024-01-17 00:15:17 +0530

    Initial commit

--- README.md
+++ README.md
@@ -1,1 +1,52 @@
-# Portfolio
+<h1 align="center">Project Name - Portfolio Website</h1>
+
+<!-- ABOUT THE PROJECT -->
+
+## About The Project
+Welcome to the repository for my personal portfolio website! This website serves as a showcase of my skills, projects, and experiences. Feel free to explore the live website and check out the demo video to get a glimpse of the features and design.
+
+## Table of Contents
+
+- [About The Project](#about-the-project)
+- [Table of Contents](#table-of-contents)
+- [Demo Video](#demo-video)
+- [Live Website](#live-website)
+- [Description](#description)
+- [Technologies Used](#technologies-used)
+- [What I've Learned](#what-ive-learned)
+- [Contributing](#contributing)
+
+## Demo Video
+
+Watch the demo video to see the portfolio website in action: 
+
+## Live Website
+
+Visit the live website to explore my portfolio: [Website](#)
+
+## Description
+
+This portfolio website is designed to showcase my skills, projects, and experiences in a visually appealing and user-friendly manner. It provides an overview of my background, a portfolio section featuring various projects, and a contact form for easy communication.
+
+## Technologies Used
+
+The website is built using the following technologies:
+
+-   ![JavaScript](https://img.shields.io/badge/javascript-%23323330.svg?style=for-the-badge&logo=javascript&logoColor=%23F7DF1E)
+-   ![SASS](https://img.shields.io/badge/SASS-hotpink.svg?style=for-the-badge&logo=SASS&logoColor=white)
+-   ![HTML](https://img.shields.io/badge/HTML5-E34F26?style=for-the-badge&logo=html5&logoColor=white)
+
+## What I've Learned
+
+While working on this project, I have gained experience in:
+
+- Creating responsive and aesthetically pleasing web pages using HTML5 and CSS3.
+- Implementing interactive features and animations using JavaScript.
+- Incorporating a map component to display location or project-related information.
+- Enhancing user experience through thoughtful design and navigation.
+
+## Contributing
+
+If you find any issues or have suggestions for improvement, feel free to open an issue or create a pull request. Contributions are welcome!
+
+Thank you for visiting my portfolio repository! If you have any questions or feedback, feel free to reach out. Happy coding!
---
+++ js/config.js
@@ -0,0 +1,81 @@
+export const statsData = [
+  { totalNo: "400+", catagories: "Total Commits" },
+  { totalNo: "5+", catagories: "Languages" },
+  { totalNo: "1", catagories: "Internship" },
+  { totalNo: "20+", catagories: "projects" },
+];
+
+export const skillData = [
+  { icon: "react", language: "React", percentage: "40%" },
+  { icon: "js", language: "Javascrip", percentage: "80%" },
+  { icon: "python", language: "Python", percentage: "80%" },
+  { icon: "phoenix-framework", language: "SQL(DataBase)", percentage: "50%" },
+  { icon: "html5", language: "HTML5", percentage: "100%" },
+  { icon: "css3-alt", language: "CSS", percentage: "100%" },
+];
+
+export const timelineData = [
+  {
+    timePeriod: "Feb'23 - Present",
+    position: "Member",
+    company: "FOSSCU",
+    about: `People at FOSSCU (Free and Open-Source Community United ) are a Group of individuals who are constantly working to promote Open-Source Culture.`,
+  },
+  {
+    timePeriod: "March'23 - Present",
+    position: "Member (Web Domain)",
+    company: "GDSC KIET",
+    about: `a club open to all undergraduate and graduate students with an interest in learning about what it means to be a developer. By joining our chapter, club members learn what it means to form the bridge between the theory and concepts taught in classes and practice with industry developer tools. `,
+  },
+  {
+    timePeriod: "March'23 - Nov'23",
+    position: "Web Developer",
+    company: "Ambuvians HealthCare pvt. ltd.",
+    about: `your destination for healthcare and emergency medical services. Our mission is to provide exceptional care promptly and empathetically. As a dedicated ambulance service, we offer rapid assistance to those in need. Led by skilled medical professionals and advanced technology, we lead in medical transportation and emergency response. Our unwavering commitment to excellence makes us a reliable choice during crises. `,
+  },
+  {
+    timePeriod: "Dec'22 - July'23",
+    position: "Member",
+    company: "Innogeeks KIET",
+    about: `a remarkable joint initiative by both faculty and students, strives to elevate our institution and align it with esteemed national and international counterparts. - Focussing on empowering students to excel beyond their academic knowledge, honing their skills for real-world challenges.`,
+  },
+];
+
+export const projectData = [
+  {
+    imageURL: "ambuvians.png",
+    name: "Ambuvians Website",
+    website: "https://ambuvians.vercel.app/",
+    github: "https://github.com/onlyVishesh/ambuvians",
+  },
+  {
+    imageURL: "weatherApp.png",
+    name: "Weather App",
+    website: "https://weather-app-onlyvishesh.vercel.app/",
+    github: "https://github.com/onlyVishesh/Weather-App",
+  },
+  {
+    imageURL: "diamondClicker.png",
+    name: "Diamond Clicker Game",
+    website: "https://diamond-clicker-game.vercel.app/",
+    github: "https://github.com/onlyVishesh/Diamond-Clicker-Game",
+  },
+  {
+    imageURL: "meetClone.png",
+    name: "Meet Clone",
+    website: "https://meet-clone-five.vercel.app/",
+    github: "https://github.com/onlyVishesh/Meet-Clone",
+  },
+  {
+    imageURL: "navbar.png",
+    name: "Navigation bars",
+    website: "https://navbars-zeta.vercel.app/",
+    github: "https://github.com/onlyVishesh/Navbars",
+  },
+  {
+    imageURL: "newsApp.png",
+    name: "News App",
+    website: "https://newspress-zeta.vercel.app/",
+    github: "https://github.com/onlyVishesh/NewsPress",
+  },
+];
