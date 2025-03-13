a1762f60ec04f4df2aa55d167edef610c0f290c2
onlyVishesh <visheshmaurya14@gmail.com>
2025-03-14 01:36:05 +0530

    Added page

---
+++ app/assets/icons/HeroSvg.tsx
@@ -0,0 +1,16 @@
+import Image from "next/image";
+
+export default function HeroSvg() {
+  return (
+    <Image
+      className="rounded-2xl mb-4 object-cover max-h-96 min-h-96 bg-top"
+      src="/terminal.gif"
+      width={600}
+      height={420}
+      quality={100}
+      alt={"terminal"}
+      // placeholder="blur"
+      priority
+    />
+  );
+}
