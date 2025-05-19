30872ea4818207b86805970a52d3548b7ea21322
onlyVishesh <visheshmaurya14@gmail.com>
2025-05-19 19:13:42 +0530

    added files

---
+++ app/components/shared/HashScroll.tsx
@@ -0,0 +1,33 @@
+import Link from "next/link";
+
+type props = {
+  text: React.ReactNode;
+  event?: any;
+};
+
+export const slugify = (id: any) => {
+  if (id) {
+    id.toString()
+      .toLowerCase()
+      .replaceAll(/[^-\w]+/g, "-")
+      .replaceAll(/--+/g, "-")
+      .replace(/^-|-$/g, "");
+  }
+  return "";
+};
+
+export default function HashScroll({ text, event }: props) {
+  return (
+    <Link
+      onClick={event}
+      href={`#${text
+        ?.toString()
+        .toLowerCase()
+        .replaceAll(/[^-\w]+/g, "-")
+        .replaceAll(/--+/g, "-")
+        .replace(/^-|-$/g, "")}`}
+    >
+      {text}
+    </Link>
+  );
+}
